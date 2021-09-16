81bfd9d0dc6252cba0e3c2d481d2776a002d5b9e
Akshay Kumar T N <akshay.t@tvsnext.io>
2021-09-16 18:00:08 +0530

    [Akshay] Added : InputBox for password and Validation is done for ResetPassword form

--- src/app/components/reset-password/reset-password.component.ts
+++ src/app/components/reset-password/reset-password.component.ts
@@ -17,6 +17,8 @@ export class ResetPasswordComponent implements OnInit {
     this.ResetPasswordForm = new FormGroup(
       {
         Email : new FormControl('',[Validators.required, Validators.pattern('^([a-z]){1,}[a-z0-9]*([.+_-]){0,1}[0-9a-z]+(@){1}([0-9a-z]+)(\\.([a-z]){2,}){1}(\\.[a-z]{2,})?$'),Validators.minLength(6)]),
+        Password : new FormControl('',[Validators.required, Validators.pattern('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?!.*\\W\\w*\\W)(?!.*\\s).{8,}$'),Validators.minLength(8)]),
+        ConfirmPassword : new FormControl('',[Validators.required]),
       }
     )
   }
@@ -24,6 +26,15 @@ export class ResetPasswordComponent implements OnInit {
   getEmailInvalidMessage() {
     return this.ResetPasswordForm.get("Email")?.hasError('required') ? 'You must enter a value' :
     this.ResetPasswordForm.get("Email")?.hasError('pattern') ? 'Not a valid email' : '';
+  }
+  getPasswordInvalidMessage() {
+    if (this.ResetPasswordForm.get("Password")?.hasError("required")) {
+      return ("Password is required"); 
+    }
+    else if (this.ResetPasswordForm.get("Password")?.hasError("minlength")) {
+      return "Password must be 8 characters";
+    }
+    return null;  
   }
 
 }
