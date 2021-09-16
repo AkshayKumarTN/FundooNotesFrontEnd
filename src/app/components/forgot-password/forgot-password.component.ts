719fe2dea252a92fa798e84288236b066f110200
Akshay Kumar T N <akshay.t@tvsnext.io>
2021-09-16 18:42:31 +0530

    [Akshay] Added : FortgotPassword and ResetPassword Form Validation

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
+  ForgotPasswordForm!: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.ForgotPasswordForm = new FormGroup(
+      {
+        Email : new FormControl('',[Validators.required, Validators.pattern('^([a-z]){1,}[a-z0-9]*([.+_-]){0,1}[0-9a-z]+(@){1}([0-9a-z]+)(\\.([a-z]){2,}){1}(\\.[a-z]{2,})?$'),Validators.minLength(6)])
+      }
+    )
+  }
+
+  getEmailInvalidMessage() {
+    return this.ForgotPasswordForm.get("Email")?.hasError('required') ? 'You must enter a value' :
+    this.ForgotPasswordForm.get("Email")?.hasError('pattern') ? 'Not a valid email' : '';
   }
 
 }
