17a4241618128d4b043359ce4bf672e02aba3d7a
Akshay Kumar T N <akshay.t@tvsnext.io>
2021-09-19 21:02:11 +0530

    [Akshay] Added : Profile PopUp Data And SideNavBar functionality in Dashboard page

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,25 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
+
 export class DashboardComponent implements OnInit {
 
   isExpandable: boolean = false;
   mobileQuery: MediaQueryList;
-
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) 
-  {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-
-   }
+  private _mobileQueryListener: () => void;
+  
+  FirstName :string = JSON.parse((localStorage.getItem('FundooNotes'))!).FirstName;
+  LastName :string = JSON.parse((localStorage.getItem('FundooNotes'))!).LastName;
+  Email :string= JSON.parse((localStorage.getItem('FundooNotes'))!).Email;
   
 
+
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+     media: MediaMatcher,
+     private routers: Router
+     ){
+
+      this.mobileQuery = media.matchMedia('(max-width: 600px)');
+      this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+      
+   }
+
+   
   ngOnInit(): void {
+  }
+
+  logout() {
+    
+    this.routers.navigateByUrl('login');
   }
 
 }
